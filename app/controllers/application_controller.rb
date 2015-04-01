2a4378fc9b1835092fc2e278e9df007c3152dd50
stefanosalvucci <stefano.salvucci@gmail.com>
2015-04-01 02:24:44 +0200

    spizza goes well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, except: [:spizza]
+  require 'open-uri'
 
   def welcome
 
   end
+
+  def suggest
+    render json: JSON.load(open("http://localhost:8983/solr/new_core/suggest?q=#{params[:q]}&wt=json&indent=true"))['spellcheck']['suggestions'][1]['suggestion'].map{|v| {value: v}}
+  end
+
+  def spizza
+    @docs = JSON.load(open("http://localhost:8983/solr/new_core/select?q=#{params[:q]}&wt=json&indent=true&rows=#{(500..700).to_a.sample}"))['response']['docs']
+  end
+
 end
